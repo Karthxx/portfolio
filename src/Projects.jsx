a94ce81d9b29038b1c7ab2b8e82517d4a373dc02
Karthik I <karthik3105@gmail.com>
2024-08-18 17:32:59 +0100

    added fade in animation to projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,14 +1,69 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import SMB from "./assets/ProjectsImgs/SMB.jpg";
 import Hotstar from "./assets/ProjectsImgs/Hotstar.jpg";
 import Insta from "./assets/ProjectsImgs/Insta.png";
 const Projects = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.3,
+        delayChildren: 0.2,
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
     <div className="max-w-5xl overflow-x-hidden">
-      <div>
-        <h2 className="text-lg font-bold text-center mb-8">Projects</h2>
-      </div>
-      <div className="w-full ">
-        <div className="flex overflow-x-auto space-x-5 p-4 snap-x snap-mandatory scroll-px-4 no-scrollbar">
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={containerVariants}
+      >
+        <motion.h2
+          className="text-lg font-bold text-center mb-8"
+          variants={childVariants}
+        >
+          Projects
+        </motion.h2>
+      </motion.div>
+      <motion.div
+        className="w-full"
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={containerVariants}
+      >
+        <motion.div
+          variants={childVariants}
+          className="flex overflow-x-auto space-x-5 p-4 snap-x snap-mandatory scroll-px-4 no-scrollbar"
+        >
           <div className="border rounded-md w-64 md:w-72 p-4 text-center flex-shrink-0 snap-start transition-transform duration-300 ease-in-out flex justify-center flex-col">
             <img src={SMB} alt="" className="h-32" />
             <h3 className="my-5 text-base font-semibold">
@@ -56,8 +111,8 @@ const Projects = () => {
               </button>
             </a>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
